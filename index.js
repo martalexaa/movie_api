8b9be6ec2bfa2684ddbfbf41304277cbfa6846b8
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-01-20 20:51:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require('express-validator');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const CONNECTION_URI = process.env;
 
 const Movies = Models.Movie;
 const Users = Models.User;
