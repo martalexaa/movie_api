1c51f5579b8766583bb4b73db3cc806569e3c765
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-01-21 18:43:37 +0100

    index.js update

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require('express-validator');
-const dotenv = require("dotenv");
-dotenv.config();
+//const dotenv = require("dotenv");
+//dotenv.config();
 
-const CONNECTION_URI = process.env;
+//const CONNECTION_URI = process.env;
 
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -16,7 +16,7 @@ mongoose.set('strictQuery', true);
 
 
 //connect to online database using enviormental variable
-mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( 'mongodb+srv://username:password@cluster0.loahamq.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const express = require('express'),
