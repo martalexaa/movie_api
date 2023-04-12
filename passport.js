00be60d4fff019a6396fec7766af8be56a548680
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-04-12 19:12:30 +0200

    Added JSDoc

--- passport.js
+++ passport.js
@@ -7,49 +7,81 @@ let Users = Models.User,
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-  passport.use(
-    new LocalStrategy(
-      {
-        usernameField: "Username",
-        passwordField: "Password",
-      },
-      (username, password, callback) => {
-        console.log(username + "  " + password);
-        Users.findOne({ Username: username }, (error, user) => {
-          if (error) {
-            console.log(error);
-            return callback(error);
-          }
-  
-          if (!user) {
-            console.log("Incorrect username or password.");
-            return callback(null, false, {
-              message: "Incorrect username or password.",
-            });
-          }
-  
-          if (!user.validatePassword(password)) {
-            console.log("incorrect password");
-            return callback(null, false, {
-              message: "Incorrect password or username.",
-            });
-          }
-          console.log("finished");
-          return callback(null, user);
-        });
-      }
-    )
-  );
+/**
+ * Passport LocalStrategy
+ *
+ * @param {Object} options - The options for LocalStrategy.
+ * @param {string} options.usernameField - The field name for username in the request body.
+ * @param {string} options.passwordField - The field name for password in the request body.
+ * @param {function} verify - The verify callback function.
+ */
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    /**
+     * Verify callback for LocalStrategy
+     *
+     * @param {string} username - The username provided in the request body.
+     * @param {string} password - The password provided in the request body.
+     * @param {function} callback - The callback function to invoke after verification.
+     */
+    (username, password, callback) => {
+      console.log(username + "  " + password);
+      Users.findOne({ Username: username }, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
 
-passport.use(new JWTStrategy({
-    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
-}, (jwtPayload, callback) => {
-    return Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+        if (!user) {
+          console.log("Incorrect username or password.");
+          return callback(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        if (!user.validatePassword(password)) {
+          console.log("incorrect password");
+          return callback(null, false, {
+            message: "Incorrect password or username.",
+          });
+        }
+        console.log("finished");
+        return callback(null, user);
+      });
+    }
+  )
+);
+
+/**
+ * Passport JWTStrategy
+ *
+ * @param {Object} options - The options for JWTStrategy.
+ * @param {function} verify - The verify callback function.
+ */
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'your_jwt_secret'
+    },
+    /**
+     * Verify callback for JWTStrategy
+     *
+     * @param {Object} jwtPayload - The payload extracted from JWT.
+     * @param {function} callback - The callback function to invoke after verification.
+     */
+    (jwtPayload, callback) => {
+      return Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
